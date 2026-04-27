4732147a0695b0203b7a35434d0f09529a1709ce
carmipa <paabcarmianti@gmail.com>
2026-04-27 16:57:33 -0300

    feat: implement exception handling for IP quarantine management tool to prevent system crashes during invalid user input

---
+++ atividades/atividade_3/ati_3.py
@@ -0,0 +1,169 @@
+import sys
+import ipaddress
+import os
+
+ip = []
+
+while True:
+    os.system("cls")
+    texto = """
+    ====================================================================================
+    ------------------------------ CENTRO DE OPERÇÕES SOC ------------------------------
+    ====================================================================================
+    -
+    - [1] Adicionar IP Suspeito
+    - [2] Listar IPs em Quarentena
+    - [3] Remover IP (Falso Positivo)
+    - [0] Encerrar Plantão
+    -
+    - Por favor, selecione uma das opções acima digitando o número correspondente: 
+    -
+    ====================================================================================
+    """
+
+    print(texto)
+        
+    try:
+        
+        opcao = int(input("Opção: "))
+        os.system("cls")
+
+        if opcao == 1: # Adicionar IP suspeito a quarentena       
+            
+            while True:
+                opcao1 = """
+
+                [+] ---------- Adicionar IP Suspeito a Quarentena ----------[+]      
+
+                """                
+                print(opcao1)
+                ip_quarentena = input("Digite o endereço IP suspeito: ")
+                
+                # trata a entrada de dados e a validade do ip
+                try:
+                    # usa a biblioteca do python para validadar se o ip digitado esta correto!
+                    ipaddress.ip_address(ip_quarentena)                
+                    ip.append(ip_quarentena) 
+
+                    sucesso1 = """
+
+                    [+] ---------- IP Suspeito Adicionado com Sucesso ----------[+] 
+                                                                        
+                    """
+                    print(sucesso1)
+                    print("Precione qulquer tecla para voltar ao menu principal!")
+                    input()
+                    os.system("cls") 
+                    break    
+
+                except ValueError:
+
+                    erro_validacao_ip = """
+
+                    [x] ------------- Endereço IP Inválido ------------- [x]
+                    
+                     Por favor, insira um endereço IP válido para continuar.
+                     Exemplo: 192.168.1.1
+                    
+                    [x]------------------------------------------------- [x]
+
+                    """
+                    print(erro_validacao_ip)
+            
+        elif opcao == 2: # Listar o ip suspeito da quarentena
+            
+            lista = f"""
+                [+] ---------- IPs em Quarentena ----------[+]
+
+                - {ip}
+
+            """ 
+            print(lista)
+            print("Precione qulquer tecla para voltar ao menu principal!")
+            input()
+            os.system("cls") 
+            
+        elif opcao == 3: # Remover o ip suspeito da quarentena
+
+            while True:
+
+                opcao3 = """
+
+                [+] ---------- Remover IP Suspeito ----------[+] 
+
+                """
+                print(opcao3)
+                ip_quarentena = input("Digite o endereço IP que deseja remover da quarentena: ")
+
+                try:                  
+    
+                    # trata a entrada de dados e a validade do ip
+                    ipaddress.ip_address(ip_quarentena) 
+                    # remove o ip salvo em memoria RAM    
+                    ip.remove(ip_quarentena)
+
+                    sucesso3 = f"""
+
+                        [+] ---------- IP Suspeito {ip_quarentena} Removido da Quarentena com Sucesso ----------[+] 
+                                                                            
+                        """
+                    print(sucesso3)
+                    print("Precione qualquer tecla para voltar ao menu principal!")
+                    input()
+                    os.system("cls")
+                    break
+
+                except ValueError:
+                    erro_validacao_ip = """
+
+                        [x] ------------- Endereço IP Inválido ------------- [x]
+                        
+                        Por favor, insira um endereço IP válido para continuar.
+                        Exemplo: 192.168.1.1
+                        
+                        [x]------------------------------------------------- [x]
+
+                        """
+                    print(erro_validacao_ip)
+                
+        elif opcao == 0:   # encerra o programa
+                
+            sair = """
+                [x]------------- Encerrar Plantão ------------- [x]
+            """
+            print(sair)
+            sys.exit()
+
+        else:
+            # trata opções inválidas de entrada no menu principal
+            invalido = """
+            [!]------------- Opção inválida ------------- [!]
+            
+             Por favor escolha uma opçao válida de 1 à 4
+            
+            [!]-------------------------------------------[!]
+            
+            """
+            print(invalido)
+            input("Pressione ENTER para tentar novamente...")   
+
+    except ValueError:
+        erro1 = """
+            [x]------------- Erro de Entrada ------------- [x]
+            
+             Por favor digite apenas números de 1 a 4   
+            
+            [x]-------------------------------------------[x]
+            
+            """
+        print(erro1)
+        input("Pressione ENTER para tentar novamente...")
+        
+        
+
+
+
+
+
+
+
